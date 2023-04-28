49f50895c8f2ce989d4120a892580eca54460e75
Christopher Carrasco <christopher3carrasco@gmail.com>
2023-04-28 01:10:03 -0500

    feat: add getCoords helper

--- src/helpers/getCoords.js
+++ src/helpers/getCoords.js
@@ -1,3 +1,19 @@
-export const getCoords = () => {
-  return {};
+export const getCoords = (latitude, longitude) => {
+  const earthRadius = 6378137;
+  const radiusInMeters = 222;
+
+  const latRadius = radiusInMeters / earthRadius;
+  const lngRadius =
+    radiusInMeters / (earthRadius * Math.cos((Math.PI * latitude) / 180));
+
+  const minLat = latitude - (latRadius * 180) / Math.PI;
+  const maxLat = latitude + (latRadius * 180) / Math.PI;
+  const minLng = longitude - (lngRadius * 180) / Math.PI;
+  const maxLng = longitude + (lngRadius * 180) / Math.PI;
+
+  const urlCoords =
+    `https://www.openstreetmap.org/export/embed.html` +
+    `?bbox=${minLng},${minLat},${maxLng},${maxLat}&layer=mapnik` +
+    `&marker=${latitude},${longitude}`;
+  return urlCoords;
 };
