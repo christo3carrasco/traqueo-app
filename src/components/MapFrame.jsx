901b16063acbcedc8f000840662726676f6c3975
Christopher Carrasco <christopher3carrasco@gmail.com>
2023-04-28 01:41:01 -0500

    feat: add MapFrame component

--- src/components/MapFrame.jsx
+++ src/components/MapFrame.jsx
@@ -1,7 +1,26 @@
-export const MapFrame = () => {
+import PropTypes from "prop-types";
+
+import { Col, Container, Row } from "react-bootstrap";
+import { getCoords } from "../helpers/getCoords";
+
+export const MapFrame = ({ latitude, longitude }) => {
   return (
-    <>
-      <h3>MapFrame</h3>
-    </>
+    <Container>
+      <Row>
+        <Col>
+          <iframe
+            width="100%"
+            height="400"
+            src={getCoords(latitude, longitude)}
+            style={{ border: "5px solid #f5434a", borderRadius: "10px" }}
+          />
+        </Col>
+      </Row>
+    </Container>
   );
+};
+
+MapFrame.propTypes = {
+  latitude: PropTypes.number.isRequired,
+  longitude: PropTypes.number.isRequired,
 };
