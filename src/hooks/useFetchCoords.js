48b20cb551338a026cc6fc2161b6482b3cb757a5
LuisSilvaB <luissilvaballadares@hotmail.com>
2023-04-30 17:41:08 -0500

    Update : useFetchCoords

--- src/hooks/useFetchCoords.js
+++ src/hooks/useFetchCoords.js
@@ -1,3 +1,22 @@
+import { useEffect, useState } from "react";
 export const useFetchCoords = () => {
-  return {};
+  const [coordenadas, setCoordenadas] = useState([]); 
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null); 
+  useEffect(()=>{
+    setIsLoading(true); 
+    fetch(`Url`)
+    .then(response => response.json()) 
+    .then(data => {
+      setCoordenadas(data); 
+      setIsLoading(false); 
+    })
+    .catch(error => {
+      setError(error); 
+      isLoading(false); 
+    })
+  },[]); 
+
+  return {coordenadas, isLoading, error};
 };
+
