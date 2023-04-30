9795638cd6b7883ceb9cb661f6a3765139fabccc
GersonVasquezG <gersonvg.1702@gmail.com>
2023-04-29 22:41:50 -0500

    Agregar estilos y quitar el Titulo de TraqueoApp.jsx

--- src/TraqueoApp.jsx
+++ src/TraqueoApp.jsx
@@ -4,7 +4,6 @@ export const TraqueoApp = () => {
   return (
     <>
       <Header />
-      <h1>TraqueoApp</h1>
       <AddCoords />
       <MapFrame latitude={-8.12689} longitude={-79.0287} />
       <Footer />
