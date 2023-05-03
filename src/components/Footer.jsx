2dd30db5e0d8086d81481c7823d65429298a105d
Christopher Carrasco <christopher3carrasco@gmail.com>
2023-05-02 20:01:00 -0500

    refactor: rename footer classname

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,23 +2,12 @@ import { Col, Container, Row } from "react-bootstrap";
 
 export const Footer = () => {
   return (
-    <>
-      <Container>
-        <Row>
-          <Col>
-            <p
-              style={{
-                margin: "8px auto 8px auto",
-                textAlign: "center",
-                color: "#FFFFFF",
-                fontWeight: "600",
-              }}
-            >
-              Copyright 2023
-            </p>
-          </Col>
-        </Row>
-      </Container>
-    </>
+    <Container>
+      <Row>
+        <Col>
+          <p className="footerText">&copy; Copyright 2023 TRAQUEO</p>
+        </Col>
+      </Row>
+    </Container>
   );
 };
