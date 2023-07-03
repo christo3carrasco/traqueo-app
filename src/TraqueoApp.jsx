3aa5e2dd96acc30770ce9f9be9380c4c1f834088
Christopher Carrasco <christopher3carrasco@gmail.com>
2023-07-02 19:23:33 -0500

    feat: add Traqueo App

--- src/TraqueoApp.jsx
+++ src/TraqueoApp.jsx
@@ -1,11 +1,27 @@
+import { useState } from "react";
 import { AddCoords, Footer, Header, MapFrame } from "./components";
 
 export const TraqueoApp = () => {
+  const [latitude, setLatitude] = useState(-8.12689);
+  const [longitude, setLongitude] = useState(-79.02871);
+
+  const onAddLatitude = (newLatitude) => {
+    if (latitude == newLatitude) return;
+    setLatitude(newLatitude);
+  };
+  const onAddLongitude = (newLongitude) => {
+    if (longitude == newLongitude) return;
+    setLongitude(newLongitude);
+  };
+
   return (
     <>
       <Header />
-      <AddCoords />
-      <MapFrame latitude={-8.12689} longitude={-79.02871} />
+      <AddCoords
+        onNewLatitude={onAddLatitude}
+        onNewLongitude={onAddLongitude}
+      />
+      <MapFrame latitude={latitude} longitude={longitude} />
       <Footer />
     </>
   );
